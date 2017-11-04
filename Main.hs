d9584081ea23fb36443daa8c07ca440f9cf89b64
Salil Ajgaonkar <ajgaonkar.salil@gmail.com>
2017-11-04 15:21:55 +0000

    Got the Client up, but not connecting to the Server correctly

--- Main.hs
+++ Main.hs
@@ -6,7 +6,7 @@ main :: IO ()
 main = do
     sock <- socket AF_INET Stream 0    -- create socket
     setSocketOption sock ReuseAddr 1   -- make socket immediately reusable - eases debugging.
-    bind sock (SockAddrInet 4242 iNADDR_ANY)   -- listen on TCP port 4242.
+    bind sock (SockAddrInet 4243 iNADDR_ANY)   -- listen on TCP port 4242.
     listen sock 2                              -- set a max of 2 queued connections
     mainLoop sock
 
@@ -19,4 +19,5 @@ mainLoop sock = do
 runConn :: (Socket, SockAddr) -> IO ()
 runConn (sock, _) = do
     send sock "Hello!\n"
+    print "Got it"
     close sock
